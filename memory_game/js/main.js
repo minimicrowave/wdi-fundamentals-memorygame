0904cd85dac16a28e206f8a6e0b62502f7f9ec92
Serene Lim <Serene@serenes-mac.local>
2018-11-22 16:21:43 +0800

    added game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -28,28 +28,38 @@ var cards = [
 var cardsInPlay = [];
 
 var checkForMatch = function(){
-	if (cardsInPlay.length === 2) {
 		if (cardsInPlay[0] === cardsInPlay[1]) {
 			alert("You found a match!");
 		}
 		else {
 			alert("Sorry, try again.");
 		}
-	}
 }
 
-var flipCard = function(cardId) {
+var flipCard = function() {
+	var cardId = this.getAttribute('data-id');
 	cardsInPlay.push(cards[cardId].rank);
-	checkForMatch();
+	this.setAttribute('src', cards[cardId].cardImage);
+	
+	if (cardsInPlay.length === 2) {
+		checkForMatch();
+	}
 	console.log("User flipped " +  cards[cardId].rank);
 	console.log(cards[cardId].suit);
 	console.log(cards[cardId].cardImage);
 
 }
 
+var createBoard = function() {
+	for (var i=0; i<cards.length; i++) {
+		var cardElement = document.createElement('img');
+		cardElement.setAttribute('src', 'images/back.png');
+		cardElement.setAttribute('data-id', i);
+		cardElement.addEventListener('click', flipCard);
+		document.getElementById('game-board').appendChild(cardElement);
+	}
+}
 
 
-flipCard(2);
-flipCard(3);
-
+createBoard();
 
